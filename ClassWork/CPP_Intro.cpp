6aa2eb35d42226dddffb801c9a896390b1f9cac3
Annonymous <dev@rivne.itstep.org>
2020-11-22 12:25:22 +0200

    First classwork

---
+++ ClassWork/CPP_Intro.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+
+using namespace std;
+/*
+class Point
+{
+private:
+	int x;
+	int y;
+public:
+
+	void OutPut()
+	{
+		cout << "X = " << x << endl;
+		cout << "Y = " << y << endl;
+	}
+
+	void FillCoords()
+	{
+		cout << "Enter x coord -> ";
+		cin >> x;
+		cout << "Enter y coord -> ";
+		cin >> y;
+	}
+
+	void Set_X(int x)
+	{
+		this->x = x;
+	}
+
+	void Set_Y(int y)
+	{
+		this->y = y;
+	}
+};
+*/
+
+
+int main()
+{
+	/*
+	Point First;
+	int x, y;
+
+	int choice;
+	while (true)
+	{
+		cout << "1.Fill coords" << endl
+			<< "2.Output coords" << endl
+			<< "3.Change x coord" << endl
+			<< "4.Change y coord" << endl
+			<< "0.Exit" << endl;
+		cout << "Choice-> "; cin >> choice;
+		switch (choice)
+		{
+		case 1:
+		{
+			First.FillCoords();
+			break;
+		}
+		case 2:
+		{
+			First.OutPut();
+			break;
+		}
+		case 3:
+		{
+			cout << "Enter new x-> "; cin >> x;
+			First.Set_X(x);
+			break;
+		}
+		case 4:
+		{
+			cout << "Enter new y-> "; cin >> y;
+			First.Set_Y(y);
+			break;
+		}
+		case 0:
+		{
+			exit(0);
+		}
+		default:
+		{
+			cout << "Invalid code!" << endl;
+		}
+		}
+	}
+	return 0;*/
+
+
+}
