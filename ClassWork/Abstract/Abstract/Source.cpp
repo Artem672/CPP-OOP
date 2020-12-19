6a5f33a45dfa4acd7aaf906f21da995e356ff9a7
Artem672 <synytsiaartem07@gmail.com>
2020-12-19 22:52:47 +0200

    Part of 20.12 homework

--- ClassWork/Abstract/Abstract/Source.cpp
+++ ClassWork/Abstract/Abstract/Source.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <windows.h>
+#include <ctime>
 
 using namespace std;
 
@@ -12,17 +14,17 @@ private:
 	private:
 		double Width;
 		double Heigth;
-		int Floor;
+		unsigned int Floor;
 	public:
 
-		Room(double Width, double Heigth, int Floor)
+		Room(double Width, double Heigth, unsigned int Floor)
 		{
 			this->Width = Width;
 			this->Heigth = Heigth;
 			this->Floor = Floor;
 		}
 
-		double RoomSquare()
+		double GetRoomSquare()
 		{
 			return (Width * Heigth);
 		}
@@ -37,7 +39,7 @@ private:
 			this->Heigth = Heigth;
 		}
 
-		void SetFloor(int Floor)
+		void SetFloor(unsigned int Floor)
 		{
 			this->Floor = Floor;
 		}
@@ -52,7 +54,7 @@ private:
 			return Heigth;
 		}
 
-		int GetFloor()
+		unsigned int GetFloor()
 		{
 			return Floor;
 		}
@@ -78,27 +80,109 @@ private:
 	};
 
 public:
-
-	int RoomNumbers;
-	int FloorNumbers;
+	int RoomNumber = 0;
 	Room Kitchen;
-	Room* rooms = new Room[RoomNumbers];
+	Room* Rooms;
+	int FloorNumber = 0;
 
-	House(int RoomNumbers, int FloorNumbers)
+	House()
 	{
-		this->RoomNumbers = RoomNumbers;
-		this->FloorNumbers = FloorNumbers;
+		while (this->FloorNumber <= 0 || this->RoomNumber <= 0)
+		{
+
+			cout << "Enter number of floors-> ";
+			cin >> this->FloorNumber;
+			cout << "Enter number of rooms-> ";
+			cin >> this->RoomNumber;
+
+			if (this->FloorNumber <= 0 || this->RoomNumber <= 0)
+			{
+				cout << "\nSomething wrong! Try one more time!" << endl << endl;
+			}
+			else if (this->FloorNumber > 0 && this->RoomNumber > 0)
+			{
+				this->Rooms = new Room[this->RoomNumber];
+				cout << "\nData were entered correctly!" << endl;
+			}
+		}
 	}
 
 	~House()
 	{
-		delete[] rooms;
+		delete[] this->Rooms;
 	}
-	
 
+	int TotalRoomSquare()
+	{
+		int RoomSquare = 0;
+
+		for (auto i = 0; i < this->RoomNumber; i++)
+		{
+			RoomSquare += this->Rooms[i].GetRoomSquare();
+		}
+
+		return RoomSquare;
+	}
 };
 
 int main()
 {
-	
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	srand((unsigned)time(NULL));
+
+	House house;
+	int Menu = 0;
+
+	while (true)
+	{
+		cout << endl << "~~Your functions~~" << endl
+			<< "0. Exit " << endl
+			<< "1. Information about house rooms" << endl
+			<< "2. Output total room square" << endl
+			<< "3. Output total room square by floor" << endl
+			<< "4. Change room properties" << endl
+			<< "5. Change kitchen properties" << endl
+			<< "6. Output information about rooms on the floor" << endl
+			<< "Your choice-> ";
+		cin >> Menu;
+
+		switch (Menu)
+		{
+		case 0:
+		{
+			exit(0);
+			break;
+		}
+		case 1:
+		{
+			break;
+		}
+		case 2:
+		{
+			break;
+		}
+		case 3:
+		{
+			break;
+		}
+		case 4:
+		{
+			break;
+		}
+		case 5:
+		{
+			break;
+		}
+		case 6:
+		{
+			break;
+		}
+		default:
+		{
+			cout << "Invalid code!" << endl;
+			break;
+		}
+		}
+	}
 }
